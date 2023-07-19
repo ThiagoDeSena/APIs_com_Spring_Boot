22d413f02881c76b0b9d64dbb886609c4a757c1b
Thiago de sena lima <thiagosena316@gmail.com>
2023-07-19 15:23:46 -0300

    Ordenar Registros

--- src/main/java/br/com/projeto/api/controle/Controle.java
+++ src/main/java/br/com/projeto/api/controle/Controle.java
@@ -1,11 +1,14 @@
 
 package br.com.projeto.api.controle;
 
-import org.hibernate.mapping.List;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,8 +28,34 @@ public class Controle {
     }
 
     @GetMapping("/api")
-    public List<Pessoa> selecionar(){
+    public Iterable<Pessoa> selecionar(){   //List ivés de Iterable
         return acao.findAll();
+    }
+
+    @GetMapping("/api/{codigo}")
+    public Pessoa selecionarPeloCodigo(@PathVariable int codigo){
+        return acao.findByCodigo(codigo);
+    }
+
+    @PutMapping("/api")
+    public Pessoa editar(@RequestBody Pessoa obj){
+        return acao.save(obj);
+    }
+
+    @DeleteMapping("/api/{codigo}")
+    public void remover(@PathVariable int codigo){
+        Pessoa obj = selecionarPeloCodigo(codigo);
+        acao.delete(obj);
+    }
+
+    @GetMapping("/api/contador")
+    public long contador(){
+        return acao.count();
+    }
+
+    @GetMapping("/api/ordenarNomes")
+    public List<Pessoa> ordenarNomes(){
+        return acao.findByOrderByNomeDesc();
     }
 
     @GetMapping("/")
--- src/main/java/br/com/projeto/api/repositorio/Repositorio.java
+++ src/main/java/br/com/projeto/api/repositorio/Repositorio.java
@@ -11,4 +11,8 @@ import br.com.projeto.api.modelo.Pessoa;
 public interface Repositorio extends CrudRepository<Pessoa, Integer>{
     
     List<Pessoa> findAll();
+
+    Pessoa findByCodigo(int codigo);
+
+    List<Pessoa> findByOrderByNomeDesc();
 }
